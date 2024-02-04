36f6b2322d719352da02d2396e80c536850769ab
Muhammad Umer Farooq <digitel966@gmail.com>
2024-02-05 01:09:53 +0500

    desktop ready

---
+++ article-preview-component-master/js/script.js
@@ -0,0 +1,16 @@
+var shareButton = document.getElementById('share');
+var sharebarBtn = document.getElementById('sharebarId');
+
+shareButton.addEventListener('click',shareBar);
+
+
+function shareBar(e){
+
+sharebarBtn.classList.toggle( 'active');
+
+console.log(sharebarBtn);
+
+
+  
+}
+
