ea90331817b71bd416b8a13b6571f63281f120fd
Muhammad Umer Farooq <digitel966@gmail.com>
2024-02-06 02:26:56 +0500

    ..

--- article-preview-component-master/js/script.js
+++ article-preview-component-master/js/script.js
@@ -1,16 +1,17 @@
-var shareButton = document.getElementById('share');
-var sharebarBtn = document.getElementById('sharebarId');
+var shareButton = document.getElementById("share");
+var sharebarBtn = document.getElementById("sharebarId");
+var detailbtn = document.getElementById("detailId");
+var shareBarMb = document.getElementById("share-bar-mbId");
 
-shareButton.addEventListener('click',shareBar);
+shareButton.addEventListener("click", shareBar);
 
+function shareBar(e) {
+  sharebarBtn.classList.toggle("active");
+  shareButton.classList.toggle("active");
+  detailbtn.classList.toggle("active");
 
-function shareBar(e){
+  //   show mobile share bar
+  shareBarMb.classList.toggle("active");
 
-sharebarBtn.classList.toggle( 'active');
-
-console.log(sharebarBtn);
-
-
-  
+  console.log(detailbtn);
 }
-
