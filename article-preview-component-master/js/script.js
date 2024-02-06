0b512a97892342e0a8f800da971792eb8e980a86
Muhammad Umer Farooq <digitel966@gmail.com>
2024-02-06 14:03:47 +0500

    final commit

--- article-preview-component-master/js/script.js
+++ article-preview-component-master/js/script.js
@@ -1,17 +1,23 @@
 var shareButton = document.getElementById("share");
 var sharebarBtn = document.getElementById("sharebarId");
-var detailbtn = document.getElementById("detailId");
-var shareBarMb = document.getElementById("share-bar-mbId");
 
 shareButton.addEventListener("click", shareBar);
 
 function shareBar(e) {
   sharebarBtn.classList.toggle("active");
   shareButton.classList.toggle("active");
-  detailbtn.classList.toggle("active");
+}
 
-  //   show mobile share bar
-  shareBarMb.classList.toggle("active");
+var mbShareBtn = document.getElementById("footer-share");
+var mbsharebarlbtn = document.getElementById("mb-sharebarId");
+var mbdetailbtn = document.getElementById("footer-detailId");
+var footer = document.getElementById("footer");
 
-  console.log(detailbtn);
+mbShareBtn.addEventListener("click", shareFooter);
+
+function shareFooter(e) {
+  mbShareBtn.classList.toggle("active");
+  mbsharebarlbtn.classList.toggle("active");
+  mbdetailbtn.classList.toggle("active");
+  footer.classList.toggle("active");
 }
